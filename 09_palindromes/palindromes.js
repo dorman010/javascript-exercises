ab94f4708474999c88499dad13ca2f4fd2fc5edf
Uria <dorman010@gmail.com>
2022-05-21 18:42:55 +0100

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" "]/g;
+    let lowerString = string.toLowerCase();
+    let punctuationLess = lowerString.replace(regex, '');
+    let simpleString = punctuationLess.replace(/\s{2,}/g," "); //replaces blank / space characters, couldn't figure out how to add into the regex variable
+    console.log(simpleString);
 
+    let splitString = simpleString.split("");
+    let reversedArray = splitString.reverse();
+    let reversedString = reversedArray.join("");
+    console.log(reversedString);
+
+    if (simpleString == reversedString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
