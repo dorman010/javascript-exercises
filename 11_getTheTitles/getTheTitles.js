c825e0bfbd9bb2bdfcaa996d1472292f965cef00
Uria <dorman010@gmail.com>
2022-05-21 19:01:09 +0100

    solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const title = arr.map(prop => {
+        return prop.title;
+    })
+    return title;
 };
 
 // Do not edit below this line
