ac1eaa52fbc76203a4b23a367f4f9b629044a59d
Uria <dorman010@gmail.com>
2022-05-21 18:54:11 +0100

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let x = 0;
+    let y = 1;
+    if (n < 0) {
+        return 'OOPS';
+    }
+    else {
+    for (let i = 0; i < n; i++) {
+        
+        nextInteger = x+y;
+        x=y;
+        y=nextInteger;
+                
+    }
+    console.log(n);
+    console.log(x);
+    return x;}
 };
 
 // Do not edit below this line
