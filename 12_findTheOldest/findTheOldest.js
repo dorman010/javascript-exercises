5d4f37a59658e2e8bf360f4bebd0ef3076437c1d
Uria <dorman010@gmail.com>
2022-05-21 21:00:22 +0100

    solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    var currentYear = new Date();
+    var yyyy = currentYear.getFullYear();
+    people.forEach(person => {
+        if (person.yearOfDeath == undefined){
+            person.yearOfDeath = yyyy;
+            
+        }        
+    });
+    const oldest = people.sort(function(a,b){
+        const last = a.yearOfDeath - a.yearOfBirth;
+        const next= b.yearOfDeath - b.yearOfBirth;
+        return last > next ? -1 : 1;
+    });
+    console.log(currentYear);
+    console.log(yyyy);
+    return oldest[0];
 };
 
 // Do not edit below this line
